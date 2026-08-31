807571eeec456c65e5408137f2a45207058d6c81
Alwyne Batister <alwyne.batister1@gmail.com>
2026-09-01 03:29:28 +0800

    Add files via upload

---
+++ Text01.java
@@ -0,0 +1,22 @@
+public class Text01 {
+    public static void DrawTest() {
+       System.out.println("Test");
+}
+    public static void main(String[] args) {
+       System.out.println(" /  \\______/  \\");
+       //SINGLE LINE COMMENT TEST
+       System.out.println("/              \\");
+       /*MULTI-LINE COMMENT TEST
+       TEST LINE 1
+       */
+       System.out.println("(  /\\        /\\  )");
+       
+       System.out.println("=====    V    =====");
+       System.out.println("======(__|__)======");
+       System.out.println("   (            )");
+       //IT WORKS
+       System.out.println("   (____________)");
+       DrawTest();
+    }
+    
+}
